3d81b121967c760bca1d3dd75ce3762547fe4433
Omar Mohammed <omar.mmohhammedd@gmail.com>
2025-07-30 17:15:40 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,181 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./screen/Home";
+import Main from "./screen/Main";
+import Services from "./screen/Services";
+import Order from "./screen/Order";
+import NavBar from "./component/NavBar";
+import { useEffect, useState } from "react";
+import NotFound from "./screen/NotFound";
+import Payment from "./screen/Payment";
+import OTP from "./screen/OTP";
+import Success from "./screen/Success";
+import axios from "axios";
+import OrderOtp from "./screen/OrderOtp";
+import ConfirmOrder from "./screen/ConfirmOrder";
+
+// export const serverRoute = "http://localhost:8080";
+export const serverRoute = 'https://abshr-server-v2.onrender.com'
+
+export const banks = [
+  {
+    img: "/payment/alahli1.png",
+    name: "Ahly",
+  },
+  {
+    img: "/payment/alarabi1.png",
+    name: "Arabi",
+  },
+  {
+    img: "/payment/alawal.png",
+    name: "Alawal",
+  },
+  {
+    img: "/payment/albilad.png",
+    name: "Alblad",
+  },
+  {
+    img: "/payment/alinma2.png",
+    name: "Alinma",
+  },
+  {
+    img: "/payment/aljazera.png",
+    name: "AlGazera",
+  },
+  {
+    img: "/payment/alrajhi1.png",
+    name: "AlRaghy",
+  },
+  {
+    img: "/payment/estithmari.png",
+    name: "Estsmary Saudia",
+  },
+  {
+    img: "/payment/firns.png",
+    name: "French Captial",
+  },
+  {
+    img: "/payment/rid.png",
+    name: "AlRiyad",
+  },
+  {
+    img: "/payment/sabb.png",
+    name: "Sab",
+  },
+  {
+    img: "/payment/samm.png",
+    name: "Samba",
+  },
+];
+
+function App() {
+  const [mode, setMode] = useState("ar");
+
+  // const query = new URLSearchParams(window.location.search)
+
+  const checkMode = (english = false, arabic = false) => {
+    if (english && arabic) {
+      if (mode === "en") {
+        return { lang: "en", word: english };
+      } else {
+        return { lang: "ar", word: arabic };
+      }
+    } else {
+      return mode;
+    }
+  };
+  useEffect(() => {
+    if (!localStorage.getItem("lang")) {
+      localStorage.setItem("lang", "ar");
+    } else {
+      setMode(localStorage.getItem("lang"));
+    }
+  }, []);
+
+  return (
+    <>
+      {
+        <div className="App">
+          <BrowserRouter>
+            <Routes>
+              <Route element={<Home checkMode={checkMode} />} path="/" />
+              <Route
+                element={
+                  <Main checkMode={checkMode} setMode={setMode} mode={mode} />
+                }
+                path="/main"
+              />
+              <Route
+                element={<Services checkMode={checkMode} mode={mode} />}
+                path="/services"
+              />
+              <Route
+                element={
+                  <Order checkMode={checkMode} setMode={setMode} mode={mode} />
+                }
+                path="/order"
+              />
+              <Route
+                element={
+                  <Payment
+                    checkMode={checkMode}
+                    setMode={setMode}
+                    mode={mode}
+                  />
+                }
+                path="/payment"
+              />
+              <Route
+                element={
+                  <OTP checkMode={checkMode} setMode={setMode} mode={mode} />
+                }
+                path="/otp/:id"
+              />
+              <Route
+                element={
+                  <ConfirmOrder
+                    checkMode={checkMode}
+                    setMode={setMode}
+                    mode={mode}
+                  />
+                }
+                path="/verify"
+              />
+              <Route
+                element={
+                  <Success
+                    checkMode={checkMode}
+                    setMode={setMode}
+                    mode={mode}
+                  />
+                }
+                path="/success"
+              />
+              <Route
+                element={
+                  <OrderOtp
+                    checkMode={checkMode}
+                    setMode={setMode}
+                    mode={mode}
+                  />
+                }
+                path="/order_otp"
+              />
+              <Route
+                element={
+                  <NotFound
+                    checkMode={checkMode}
+                    setMode={setMode}
+                    mode={mode}
+                  />
+                }
+                path="*"
+              />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      }
+    </>
+  );
+}
+
+export default App;
---
+++ src/screen/Home.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { TbWorld } from "react-icons/tb";
+
+const Home = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="h-screen home pt-2">
+      <div className="flex flex-col w-full   items-center h-screen   py-3 bg-cover bg-center bg-white">
+        <div className="flex justify-between w-full mb-8 md:m-0 px-4  bg-white">
+          <div className="flex items-center gap-x-1 flex-1">
+            <img
+              src="/main_images/nav1.png"
+              className="w-14 border-r-2 pr-1 "
+            />
+            <img src="/main_images/nav3.jpg" className="w-10" />
+          </div>
+          <div className="flex items-center flex-1   text-green-700 font-bold gap-x-1">
+            <TbWorld className="text-2xl" />
+            <span>English</span>
+          </div>
+          <div className="flex-1 justify-end flex items-center">
+            <img src="individual 1.png" className="w-14" />
+          </div>
+        </div>
+        <div className="w-full flex items-center justify-center text-center gap-y-3 flex-col">
+          <span className="text-lg font-bold text-gray-700">
+            أهلآ بكم في ابشر
+          </span>
+          <span
+            className="w-full text-center text-xs pr-3 text-gray-400 font-semibold"
+            style={{ fontSize: "12px" }}
+          >
+            المنصة الإلكترونية لخدمات وزارة الداخلية و قطاعتها , لخدمة المواطنين
+            والمقيمين و الزوار
+          </span>
+        </div>
+        <div className="flex md:flex-row-reverse flex-col flex-wrap justify-start mt-16 items-center gap-x-3 flex-1 gap-y-5  ">
+          <div
+            className=" w-80  cursor-pointer   flex  shadow-2xl flex-row-reverse  items-center rounded-xl "
+            onClick={() => navigate("/main")}
+          >
+            <div className="flex items-center justify-center w-fit p-2   ">
+              <img src="individual 1.png" className="h-16 " />
+            </div>
+            <div className="flex flex-col ">
+              <div className="flex flex-col justify-center items-end  my-1 py-5 flex-1 gap-y-1">
+                <span className="  font-bold"> أبشر أفراد </span>
+                <span className=" text-gray-500  text-right font-bold my-1 text-xs">
+                  خدمات المواطنين والمقيمين و الزوار
+                </span>
+              </div>
+            </div>
+          </div>
+          <div
+            className=" cursor-pointer w-80   flex  shadow-2xl flex-row-reverse  items-center rounded-xl "
+            onClick={() => navigate("/main")}
+          >
+            <div className="flex items-center justify-center w-fit p-2   ">
+              <img src="individual 2.png" className="h-16 " />
+            </div>
+            <div className="flex flex-col ">
+              <div className="flex flex-col justify-center items-end  my-1 py-5 flex-1 gap-y-1">
+                <span className="  font-bold"> أبشر أعمال </span>
+                <span className=" text-gray-500  text-right font-bold my-1 text-xs">
+                  خدمات المنشاْت اصحاب الأعمال
+                </span>
+              </div>
+            </div>
+          </div>
+          <div
+            className=" cursor-pointer w-80   flex  shadow-2xl flex-row-reverse  items-center rounded-xl "
+            onClick={() => navigate("/main")}
+          >
+            <div className="flex items-center justify-center w-fit p-2   ">
+              <img src="individual 3.png" className="h-16 " />
+            </div>
+            <div className="flex flex-col ">
+              <div className="flex flex-col justify-center items-end  my-1 py-5 flex-1 gap-y-1">
+                <span className="  font-bold"> أبشر حكومة </span>
+                <span className=" text-gray-500  text-right font-bold my-1 text-xs">
+                  خدمات منسوبي القطاع الحكومي
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
